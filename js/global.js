ab6c786cd87bbb814e3281bdb48b5f5ff0c93e98
Paul Lieberman <lieb77@gmail.com>
2019-02-07 14:08:23 -0800

    initial commit to github

---
+++ js/global.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ancien = {
+    attach: function (context, settings) {
+ /* Will make menu dropdown on hover
+     $(".dropdown").hover(function() {
+       $('.dropdown-toggle', this).trigger('click'); 
+      });
+ */
+    }
+  };
+
+})(jQuery, Drupal);
